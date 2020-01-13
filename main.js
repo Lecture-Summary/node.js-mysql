b2ba7c63ff112aab472eec938fd8c752879f4174
HoJin9622 <kiss0104040@gmail.com>
2020-01-13 22:07:18 +0900

    MySQL join을 이용해서 상세보기 구현

--- main.js
+++ main.js
@@ -39,30 +39,32 @@ var app = http.createServer(function(request, response) {
         if (error) {
           throw error;
         }
-        db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(
-          error2,
-          topic
-        ) {
-          if (error2) {
-            throw error2;
-          }
-          var title = topic[0].title;
-          var description = topic[0].description;
-          var list = template.list(topics);
-          var html = template.HTML(
-            title,
-            list,
-            `<h2>${title}</h2>${description}`,
-            ` <a href="/create">create</a>
+        db.query(
+          `SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,
+          [queryData.id],
+          function(error2, topic) {
+            if (error2) {
+              throw error2;
+            }
+            console.log(topic);
+            var title = topic[0].title;
+            var description = topic[0].description;
+            var list = template.list(topics);
+            var html = template.HTML(
+              title,
+              list,
+              `<h2>${title}</h2>${description} <p>by ${topic[0].name}</p>`,
+              ` <a href="/create">create</a>
             <a href="/update?id=${queryData.id}">update</a>
             <form action="delete_process" method="post">
               <input type="hidden" name="id" value="${queryData.id}">
               <input type="submit" value="delete">
             </form>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
+            );
+            response.writeHead(200);
+            response.end(html);
+          }
+        );
       });
     }
   } else if (pathname === "/create") {
